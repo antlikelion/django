dd22d97c49f949edd6f6ce3df18e5a054e06d778
Inuh, LEE <ignorant_woo@naver.com>
2019-04-10 02:17:09 +0900

    second_modify

--- review/models.py
+++ review/models.py
@@ -7,24 +7,28 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200)
     review = models.TextField()
-    GOOD = "GOOD"
-    SOSO = "SOSO"
-    BAD = "BAD"
+    BEST = "5"
+    GOOD = "4"
+    SOSO = "3"
+    BAD = "2"
+    WORST = "1"
     SCORE_CHOICES = (
-        (GOOD, "Good"),
-        (SOSO, "So so"),
-        (BAD, "Bad"),
+        (BEST, "5"),
+        (GOOD, "4"),
+        (SOSO, "3"),
+        (BAD, "2"),
+        (WORST, "1"),
     )
     score = models.CharField(
-        max_length=4,
+        max_length=1,
         choices=SCORE_CHOICES,
         default=SOSO,
     )
     price = models.IntegerField()
     created_date = models.DateTimeField(default=timezone.now)
 
-    def is_upperclass(self):
-        return self.score in (self.GOOD, self.SOSO, self.BAD)
+    # def is_upperclass(self):
+    #     return self.score in (self.GOOD, self.SOSO, self.BAD)
 
     def __str__(self):
         return self.title
